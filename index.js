8d950cf6903ec232ba357749e03149f05b814fb5
Roman <r.yurlov@promo-bot.ru>
2020-05-14 14:16:10 +0500

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+const config = require('./config');
 
 app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000');
+  console.log(`App is running on http://localhost:${config.port}`);
 });
